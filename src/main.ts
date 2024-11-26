11d7e9b8e756c2baa732bd0a6358c67544146757
Dmitry <bdvlug@gmail.com>
2024-11-26 18:12:40 +0200

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -5,88 +5,84 @@ import { tableToCsv } from "./utils.js";
 const year = 2025;
 const month = "november";
 const startUrls = [
-    `https://www.basketball-reference.com/leagues/NBA_${year}.html`,
-    `https://www.basketball-reference.com/leagues/NBA_${year}_games-${month}.html`,
+  `https://www.basketball-reference.com/leagues/NBA_${year}.html`,
+  `https://www.basketball-reference.com/leagues/NBA_${year}_games-${month}.html`,
 ];
 
 const crawler = new CheerioCrawler({
-    async requestHandler({ $, request }) {
-        if (
-            request.url ===
-            "https://www.basketball-reference.com/leagues/NBA_2025.html"
-        ) {
-            await Promise.all([
-                KeyValueStore.setValue(
-                    "advanced-team.csv",
-                    tableToCsv($, "advanced-team"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "per_game-team.csv",
-                    tableToCsv($, "per_game-team"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "per_game-opponent.csv",
-                    tableToCsv($, "per_game-opponent"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "totals-team.csv",
-                    tableToCsv($, "totals-team"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "totals-opponent.csv",
-                    tableToCsv($, "totals-opponent"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "per_poss-team.csv",
-                    tableToCsv($, "per_poss-team"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-                KeyValueStore.setValue(
-                    "per_poss-opponent.csv",
-                    tableToCsv($, "per_poss-opponent"),
-                    {
-                        contentType: "text/csv",
-                    }
-                ),
-            ]);
-        } else if (
-            request.url ===
-            "https://www.basketball-reference.com/leagues/NBA_2025_games-november.html"
-        ) {
-            // const scheduleElem = $("#schedule");
-
-            await KeyValueStore.setValue(
-                `schedule-${month}.csv`,
-                tableToCsv($, "schedule"),
-                {
-                    contentType: "text/csv",
-                }
-            );
+  async requestHandler({ $, request }) {
+    if (
+      request.url ===
+      "https://www.basketball-reference.com/leagues/NBA_2025.html"
+    ) {
+      await Promise.all([
+        KeyValueStore.setValue(
+          "advanced-team.csv",
+          tableToCsv($, "advanced-team"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "per_game-team.csv",
+          tableToCsv($, "per_game-team"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "per_game-opponent.csv",
+          tableToCsv($, "per_game-opponent"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "totals-team.csv",
+          tableToCsv($, "totals-team"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "totals-opponent.csv",
+          tableToCsv($, "totals-opponent"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "per_poss-team.csv",
+          tableToCsv($, "per_poss-team"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+        KeyValueStore.setValue(
+          "per_poss-opponent.csv",
+          tableToCsv($, "per_poss-opponent"),
+          {
+            contentType: "text/csv",
+          }
+        ),
+      ]);
+    } else if (
+      request.url ===
+      "https://www.basketball-reference.com/leagues/NBA_2025_games-november.html"
+    ) {
+      await KeyValueStore.setValue(
+        `schedule-${month}.csv`,
+        tableToCsv($, "schedule"),
+        {
+          contentType: "text/csv",
         }
-    },
-    failedRequestHandler({ request }) {
-        console.error(`Request ${request.url} failed!`);
-    },
-    maxRequestsPerCrawl: 5,
+      );
+    }
+  },
+  failedRequestHandler({ request }) {
+    console.error(`Request ${request.url} failed!`);
+  },
+  maxRequestsPerCrawl: 5,
 });
 
 await crawler.run(startUrls);
-
-// await Dataset.exportToJSON("OUTPUT");
